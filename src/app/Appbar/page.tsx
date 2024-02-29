43f488fe2356b294685d539131e6637ff058aa6e
RIA-KALITA <kalitaria7@gmail.com>
2024-02-29 10:36:40 -0800

    advertisment

--- src/app/Appbar/page.tsx
+++ src/app/Appbar/page.tsx
@@ -2,10 +2,12 @@ import Appbar from "../component/dashboard/Appbar"
 import Footer from "../component/dashboard/Footer"
 import Subjects from "../component/dashboard/Subjects"
 
+
 export default function contact(){
     return(
          <div>
-            <Subjects/>  
+            <Subjects/> 
+            
         </div>
     )
 }
