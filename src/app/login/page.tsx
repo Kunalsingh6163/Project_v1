0366d34fe5a0dc1f2255ac4175a396d549492d1b
Poly-777 <polycarp2411@gmail.com>
2024-03-04 17:19:13 +0530

    slider

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,5 @@
-"use client";
-import React, { useEffect, useState } from "react";
+'use client';
+import React, { useEffect, useState , Component} from "react";
 import { useRouter } from "next/navigation";
 import axios from "axios";
 import toast, { Toaster } from "react-hot-toast";
@@ -20,6 +20,8 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import Logo from "../../../public/Logo.svg";
 import AIML from '../../../public/AIML.svg'
+import Carousel from '@itseasy21/react-elastic-carousel';
+import { CircleOutlined } from "@mui/icons-material";
 
 export default function LoginPage() {
   const router = useRouter();
@@ -71,122 +73,136 @@ export default function LoginPage() {
 
   const settings = {
     dots: true,
+    Slider: true,
     infinite: true,
     speed: 2000,
     slidesToShow: 1,
     slidesToScroll: 1,
+    waitForAnimate: false,
     autoplay: true,
-    autoplaySpeed: 3000,
+    autoplaySpeed: 2000,
+    customPaging: function(i:any) {
+      return (
+          <div  style={{ position: 'relative', textAlign: 'center' }}>
+              <CircleOutlined style={{ position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -150%)', color: 'white', fontSize: 24 }}>add_circle</CircleOutlined> {/* Example: Using the "add_circle" icon */}
+              <span className="slick-dots-bullet"></span>
+          </div>
+      );
+  }
   };
+
 
   return (
     <Box sx={{ flexGrow: 1 }}>
-    <Grid container spacing={2}>
-      <Grid item xs={4} >
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            justifyContent: "center",
-            height: "60vh",
-            marginTop: "170px",
-          }}
-        >
-          {loading && <Box>Loading...</Box>}
-          <Image src={Logo} alt="logo" width={200} height={100} />
+      <Grid container>
+        <Grid item xs={12} md={4} order={{xs:2,md:1}}>
+           {loading && <Box>Loading...</Box>}
 
-          <Typography
-            variant="h4"
-            fontWeight="600"
-            color="#0024E0"
-            textAlign="center"
-            fontFamily="Poppins,Sans-serif"
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              alignItems: "center",
+              justifyContent: "center",
+              marginTop:{xs:'30px'},
+              height: {xs:'100vh',md:"90%"}, // Adjust the height as needed
+              padding: "0 20px", // Add padding to the sides
+            }}
           >
-            Welcome back !
-          </Typography>
-          <Typography
-            variant="h5"
-            color="#0024E0"
-            textAlign="center"
-            fontFamily="Poppins,Sans-serif"
-          >
-            Log in to proceed with your learning
-          </Typography>
-
-          <Box sx={{ width: "80%", margin: "0 auto" }}>
-            <TextField
-              placeholder="Enter Email Id"
-              label="Email id"
-              name="email"
-              size="small"
-              required
-              focused
-              type="email"
-              value={user.email}
-              onChange={(e) => setUser({ ...user, email: e.target.value })}
-              fullWidth
-              margin="normal"
-            />
-            <TextField
-              placeholder="Enter Password"
-              label="Password"
-              name="password"
-              focused
-              required
-              type={showPassword ? "text" : "password"}
-              InputProps={{
-                endAdornment: (
-                  <IconButton
-                    aria-label="toggle password visibility"
-                    onClick={handleClickShowPassword}
-                    onMouseDown={handleMouseDownPassword}
-                  >
-                    {showPassword ? <Visibility /> : <VisibilityOff />}
-                  </IconButton>
-                ),
-              }}
-              value={user.password}
-              onChange={handlePasswordChange}
-              fullWidth
-              margin="normal"
-              error={!passwordValid}
-              helperText={
-                !passwordValid &&
-                "Password must be 8-100 characters long, contain at least one uppercase letter, one lowercase letter, and at least two digits. It should not contain spaces and blacklist specific values."
-              }
-            />
-            <Button
-              variant="contained"
-              onClick={onLogin}
-              disabled={buttonDisabled}
-              sx={{
-                boxShadow: 1,
-                maxWidth: "100%",
-                height: "41px",
-                width: "100%",
-                fontWeight: "800",
-                fontFamily: "Poppins,Sans-serif",
-                color: "white",
-              }}
-            >
-              Submit
-            </Button>
-            <Typography sx={{ textAlign: "end", color: "rgba(0, 36, 224, 1)" }}>
-              <Link sx={{ cursor: "pointer" }}> forgot password</Link>
-            </Typography>
+            <Image src={Logo} alt="logo" width={400} height={200} />
 
             <Typography
-              variant="h6"
-              align="center"
-              marginTop={1}
-              fontWeight={900}
-              sx={{ color: "blue" }}
+              variant="h4"
+              fontWeight="600"
+              color="#0024E0"
+              textAlign="center"
+              fontFamily="Poppins,Sans-serif"
             >
-              or login with
+              Welcome back !
             </Typography>
-            <Box sx={{marginTop:'25px'}}>
-            <Grid container spacing={2}
+            <Typography
+              variant="h5"
+              color="#0024E0"
+              textAlign="center"
+              fontFamily="Poppins,Sans-serif"
+            >
+              Log in to proceed with your learning
+            </Typography>
+
+            <Box sx={{ width: "80%", }}>
+              <TextField
+                placeholder="Enter Email Id"
+                label="Email id"
+                name="email"
+                size="small"
+                required
+                focused
+                type="email"
+                value={user.email}
+                onChange={(e) => setUser({ ...user, email: e.target.value })}
+                fullWidth
+                margin="normal"
+              />
+              <TextField
+                placeholder="Enter Password"
+                label="Password"
+                name="password"
+                size="small"
+                focused
+                required
+                type={showPassword ? "text" : "password"}
+                InputProps={{
+                  endAdornment: (
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={handleClickShowPassword}
+                      onMouseDown={handleMouseDownPassword}
+                    >
+                      {showPassword ? <Visibility /> : <VisibilityOff />}
+                    </IconButton>
+                  ),
+                }}
+                value={user.password}
+                onChange={handlePasswordChange}
+                fullWidth
+                margin="normal"
+                error={!passwordValid}
+                helperText={
+                  !passwordValid &&
+                  "Password must be 8-100 characters long, contain at least one uppercase letter, one lowercase letter, and at least two digits. It should not contain spaces and blacklist specific values."
+                }
+              />
+              <Button
+                variant="contained"
+                onClick={onLogin}
+                disabled={buttonDisabled}
+                sx={{
+                  boxShadow: 1,
+                  maxWidth: "100%",
+                  height: "41px",
+                  width: "100%",
+                  fontWeight: "800",
+                  fontFamily: "Poppins,Sans-serif",
+                  color: "white",
+                }}
+              >
+                Submit
+              </Button>
+              <Typography sx={{ textAlign: "end", color: "rgba(0, 36, 224, 1)" }}>
+                <Link sx={{ cursor: "pointer" }}> forgot password</Link>
+              </Typography>
+
+              <Typography
+                variant="h6"
+                align="center"
+                marginTop={1}
+                fontWeight={900}
+                sx={{ color: "blue" }}
+              >
+                or login with
+              </Typography>
+              <Grid
+              container
               sx={{ alignItems: "center", justifyContent: "space-between" }}
             >
               <Button
@@ -196,9 +212,9 @@ export default function LoginPage() {
                   background: "#FF3E30",
                   "&:hover": { background: "#FF3E30" },
                   textTransform: "none",
-                  width: 200,
+                  width:{xs:'100%',md:'200px'},
                   borderRadius: "5px",
-                  height: 41,
+                  height: '41px',
                 }}
               >
                 Google
@@ -210,19 +226,21 @@ export default function LoginPage() {
                   background: "#316FF6",
                   "&:hover": { background: "#316FF6" },
                   textTransform: "none",
-                  width: 200,
+                  width:{xs:'100%',md:'200px'},
                   borderRadius: "5px",
-                  height: 41,
+                  height: '41px',
+                  marginTop: { xs: 1, lg: 0 },
                 }}
               >
                 Facebook
               </Button>
             </Grid>
-            <Grid container spacing={2}
+            <Grid
+              container
               sx={{
                 alignItems: "center",
                 justifyContent: "space-between",
-                marginTop: 2,
+                marginTop: { xs: 1, lg: 2 },
               }}
             >
               <Button
@@ -232,9 +250,9 @@ export default function LoginPage() {
                   background: "#0077B5",
                   "&:hover": { background: "#0077B5" },
                   textTransform: "none",
-                  width: 200,
+                  width:{xs:'100%',md:'200px'},
                   borderRadius: "5px",
-                  height: 41,
+                  height: '41px',
                 }}
               >
                 Twitter
@@ -247,95 +265,96 @@ export default function LoginPage() {
                   background: "#000000",
                   "&:hover": { background: "#000000" },
                   textTransform: "none",
-                  width: 200,
+                  width:{xs:'100%',md:'200px'},
                   borderRadius: "5px",
-                  height: 41,
+                  height: '41px',
+                  marginTop: { xs: 1, lg: 0 },
                 }}
               >
                 LinkedIn
               </Button>
             </Grid>
-            </Box>
-            <Box
-              sx={{ display: "block", textAlign: "center", marginTop: "20px" }}
-            >
-              Don't have an Account ?
-              <Link
-                href="/signup"
-                sx={{ "& .MuiTypography-root": { fontWeight: "bold" } }}
+              </Box>
+              <Box
+                sx={{ display: "block", textAlign: "center", marginTop:{xs:'20px',md: "15px"} }}
               >
-                Sign Up
-              </Link>
+                Don't have an Account ?
+                <Link
+                  href="/signup"
+                  sx={{ "& .MuiTypography-root": { fontWeight: "bold" } }}
+                >
+                  Sign Up
+                </Link>
+              </Box>
             </Box>
-          </Box>
 
-          <Box
-            sx={{
-              backgroundColor: "#f0f0f0",
-              textAlign: "center",
-              padding: "10px 0",
-              marginTop: "20%",
-            }}
-          >
-            <Typography variant="body2" color="#555">
-              © 2009-2024 - inteMEgence Perk Solutions. All Rights Reserved. |
-              Privacy Policy
-            </Typography>
-          </Box>
-        </Box>
-      </Grid>
-      <Grid item xs={8} >
-        {/* <Hidden xsDown> */}
-          <Slider {...settings}>
-            <Box sx={{ width: "100%", height: "100%" }}>
+            <Box
+              sx={{
+                backgroundColor: "#f0f0f0",
+                textAlign: "center",
+                padding: "10px 0",
+                marginTop: {xs:'50px',md:"50px"},
+              }}
+            >
+              <Typography variant="body2" color="#555">
+                © 2009-2024 - inteMEgence Perk Solutions. All Rights Reserved. |
+                Privacy Policy
+              </Typography>
+            </Box>
+        </Grid>
+        <Grid item xs={12} md={8} order={{xs:1,md:2}}>
+            <Slider {...settings} arrows={false} >
+              <Box sx={{ width: "100%", height: "100%" }}>
+                <Image
+                  src="/purple.svg"
+                  alt="learning"
+                  width={1000}
+                  height={600}
+                  layout="responsive"
+                />
+              </Box>
+              <Image
+                src={"/AI.svg"}
+                alt="learning"
+                width={1000}
+                height={600}
+                layout="responsive"
+
+              />
+              <Image
+                src={"/DevOps.svg"}
+                alt="AI"
+                width={1000}
+                height={600}
+                layout="responsive"
+
+              />
+              <Image
+                src="/MernStack.svg"
+                alt="learning"
+                width={1000}
+                height={600}
+                layout="responsive"
+
+              />
+              <Image
+                src="/AIML.svg"
+                alt="AI"
+                width={1000}
+                height={600}
+                layout="responsive"
+
+              />
               <Image
                 src="/AI.svg"
                 alt="learning"
                 width={1000}
-                height={700}
+                height={600}
                 layout="responsive"
               />
-            </Box>
-            <Image
-              src={AIML}
-              alt="learning"
-              width={1000}             
-              height={700}
-              layout="responsive"
-            />
-            {/* </Box> */}
-            <Image
-              src={AIML}
-              alt="AI"
-              width={1000}
-              height={700}
-              layout="responsive"
-            />
-            <Image
-              src="/DevOps.svg"
-              alt="learning"
-              width={1000}
-              height={700}
-              layout="responsive"
-            />
-            <Image
-              src="/MernStack.svg"
-              alt="AI"
-              width={1000}
-              height={700}
-              layout="responsive"
-            />
-            <Image
-              src="/AI.svg"
-              alt="learning"
-              width={1000}
-              height={700}
-              layout="responsive"
-            />
-          </Slider>
-        {/* </Hidden> */}
+            </Slider>
+        </Grid>
       </Grid>
-    </Grid>
     </Box>
   );
 }
