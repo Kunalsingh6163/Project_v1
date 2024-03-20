07425cc9c528476d03370f28361183cd50aa9edd
NAYAN-JYOTI-ATHPORIA <nayanjyoti5i67@gmail.com>
2024-03-20 21:32:59 +0530

    test

---
+++ src/app/component/liveClass/appbar.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Appbar from "@mui/material";
+import { Box } from "@mui/system";
+export default function Liveclass(){
+    return(
+       <Box>
+        1
+       </Box>
+    )
+}
